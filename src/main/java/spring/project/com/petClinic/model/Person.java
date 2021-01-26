2abf990ec219bf8d383a04f6785e5d748a586da3
Radoslaw Piwowarczyk <rad.piwowarczyk@gmail.com>
2021-01-26 20:22:20 +0100

    created the project

---
+++ src/main/java/spring/project/com/petClinic/model/Person.java
@@ -0,0 +1,7 @@
+package spring.project.com.petClinic.model;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+}
---
+++ src/test/java/spring/project/com/petClinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.project.com.petClinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
