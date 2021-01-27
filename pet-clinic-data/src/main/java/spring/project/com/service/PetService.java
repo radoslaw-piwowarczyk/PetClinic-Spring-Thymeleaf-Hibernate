ef4b880525add1758b138c814e5935a8dbf32c5b
Radoslaw Piwowarczyk <rad.piwowarczyk@gmail.com>
2021-01-27 07:11:30 +0100

    added implementation to services

--- pet-clinic-data/src/main/java/spring/project/com/service/PetService.java
+++ pet-clinic-data/src/main/java/spring/project/com/service/PetService.java
@@ -1,4 +1,12 @@
 package spring.project.com.service;
 
+import spring.project.com.model.Pet;
+
+import java.util.Set;
+
 public interface PetService {
+
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/spring/project/com/service/VetService.java
+++ pet-clinic-data/src/main/java/spring/project/com/service/VetService.java
@@ -1,4 +1,12 @@
 package spring.project.com.service;
 
+import spring.project.com.model.Vet;
+
+import java.util.Set;
+
 public interface VetService {
+
+    Vet findById(Long id);
+    Vet save (Vet vet);
+    Set<Vet> findAll();
 }
