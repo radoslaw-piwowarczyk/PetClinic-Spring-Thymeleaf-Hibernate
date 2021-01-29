e535e81896e607a0b86cb48cb0718bfb54b9bfc2
Radoslaw Piwowarczyk <rad.piwowarczyk@gmail.com>
2021-01-29 19:33:16 +0100

    created bootstrap package and DataLoader class

---
+++ pet-clinic-web/src/main/java/spring/project/com/bootstrap/DataLoader.java
@@ -0,0 +1,10 @@
+package spring.project.com.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class DataLoader implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
