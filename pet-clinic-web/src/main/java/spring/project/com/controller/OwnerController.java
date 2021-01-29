add274692f43c197e962085c29fa78b4aac1f8ff
Radoslaw Piwowarczyk <rad.piwowarczyk@gmail.com>
2021-01-29 20:04:29 +0100

    added list of owners view in template index

--- pet-clinic-web/src/main/java/spring/project/com/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/spring/project/com/controller/OwnerController.java
@@ -1,15 +1,22 @@
 package spring.project.com.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import spring.project.com.service.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
+    private final OwnerService ownerService;
 
-    @RequestMapping({"","/","/index"})
-    public String listOwners(){
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
 
+    @RequestMapping({"", "/", "/index"})
+    public String listOwners(Model model) {
+        model.addAttribute("owners",ownerService.findAll());
         return "owners/index";
     }
 }
