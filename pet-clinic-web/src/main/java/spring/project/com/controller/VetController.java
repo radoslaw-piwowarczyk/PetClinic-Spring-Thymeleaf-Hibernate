0f5ce5b9daf387c519d8f2dfcf69d686b9e622fc
Radoslaw Piwowarczyk <rad.piwowarczyk@gmail.com>
2021-01-29 20:07:55 +0100

    reafctor in VetController

--- pet-clinic-web/src/main/java/spring/project/com/controller/VetController.java
+++ pet-clinic-web/src/main/java/spring/project/com/controller/VetController.java
@@ -1,14 +1,22 @@
 package spring.project.com.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import spring.project.com.service.VetService;
 
 @Controller
 public class VetController {
 
-    @RequestMapping({"/vets","/vets/index"})
+    private final VetService vetService;
 
-    public String listVets(){
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"/vets","/vets/index"})
+    public String listVets(Model model){
+        model.addAttribute("vets",vetService.findAll());
         return "vets/index";
     }
 }
