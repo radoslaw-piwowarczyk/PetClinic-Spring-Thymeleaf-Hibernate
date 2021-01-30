860485116ad308ed92b364180025ac1355d5c0f0
radoslaw <rad.piwowarczyk@gmail.com>
2021-01-30 22:33:20 +0100

    added Speciality class and connect it with Vet

--- pet-clinic-data/src/main/java/spring/project/com/model/Vet.java
+++ pet-clinic-data/src/main/java/spring/project/com/model/Vet.java
@@ -1,4 +1,16 @@
 package spring.project.com.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
