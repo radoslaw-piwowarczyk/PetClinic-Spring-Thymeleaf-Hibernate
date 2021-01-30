11a5f7d721e0d8981353112f619a5d1b72411a18
radoslaw <rad.piwowarczyk@gmail.com>
2021-01-30 22:29:07 +0100

    added Visit class and refactor in Owner class

--- pet-clinic-data/src/main/java/spring/project/com/model/Owner.java
+++ pet-clinic-data/src/main/java/spring/project/com/model/Owner.java
@@ -1,4 +1,16 @@
 package spring.project.com.model;
 
+import java.util.Set;
+
 public class Owner extends Person{
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
