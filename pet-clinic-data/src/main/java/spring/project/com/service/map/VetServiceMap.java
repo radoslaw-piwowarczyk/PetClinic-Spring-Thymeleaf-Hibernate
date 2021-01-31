9e9313db2124e7e79d7a21b2a357ad0b1148cf38
radoslaw <rad.piwowarczyk@gmail.com>
2021-01-31 08:50:37 +0100

    added implementation in save method in VetServiceMap

--- pet-clinic-data/src/main/java/spring/project/com/service/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/spring/project/com/service/map/VetServiceMap.java
@@ -1,13 +1,22 @@
 package spring.project.com.service.map;
 
 import org.springframework.stereotype.Service;
+import spring.project.com.model.Speciality;
 import spring.project.com.model.Vet;
+import spring.project.com.service.SpecialityService;
 import spring.project.com.service.VetService;
 
 import java.util.Set;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+
+    private final SpecialityService specialityService;
+
+    public VetServiceMap(SpecialityService specialityService) {
+        this.specialityService = specialityService;
+    }
+
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
@@ -25,6 +34,15 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet object) {
+        if (object.getSpecialities().size() > 0) {
+            object.getSpecialities().forEach(speciality -> {
+                if (speciality.getId() == null) {
+                    Speciality savedSpeciality = specialityService.save(speciality);
+                    speciality.setId(savedSpeciality.getId());
+                }
+            });
+        }
+
         return super.save(object);
     }
 
