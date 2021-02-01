58211423c17b41b1584806d22f93b8ddfa3dcb66
radoslaw <rad.piwowarczyk@gmail.com>
2021-02-01 20:41:39 +0100

    added PetSDJpaService

---
+++ pet-clinic-data/src/main/java/spring/project/com/service/springDataJpa/PetSDJpaService.java
@@ -0,0 +1,48 @@
+package spring.project.com.service.springDataJpa;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import spring.project.com.model.Pet;
+import spring.project.com.repository.PetRepository;
+import spring.project.com.service.PetService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springDataJpa")
+public class PetSDJpaService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetSDJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return petRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petRepository.deleteById(id);
+    }
+}
