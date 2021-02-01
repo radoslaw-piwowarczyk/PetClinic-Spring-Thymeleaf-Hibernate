aea4636b821561177c35566771a917b217bbd7d3
radoslaw <rad.piwowarczyk@gmail.com>
2021-02-01 20:25:18 +0100

    added OwnerSDJpaService

---
+++ pet-clinic-data/src/main/java/spring/project/com/service/springDataJpa/OwnerSDJpaService.java
@@ -0,0 +1,46 @@
+package spring.project.com.service.springDataJpa;
+
+import org.springframework.stereotype.Service;
+import spring.project.com.model.Owner;
+import spring.project.com.repository.OwnerRepository;
+import spring.project.com.service.OwnerService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class OwnerSDJpaService implements OwnerService {
+
+    private final OwnerRepository ownerRepository;
+
+    public OwnerSDJpaService(OwnerRepository ownerRepository) {
+        this.ownerRepository = ownerRepository;
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        Set<Owner> owners = new HashSet<>();
+        ownerRepository.findAll().forEach(owners::add);
+        return owners;
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return ownerRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return ownerRepository.save(object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        ownerRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        ownerRepository.deleteById(id);
+    }
+}
