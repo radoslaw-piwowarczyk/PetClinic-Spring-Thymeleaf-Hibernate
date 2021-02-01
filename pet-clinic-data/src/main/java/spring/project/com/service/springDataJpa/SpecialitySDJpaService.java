46a6d853c0b3bc73f53fabb448c0dfef505735d8
radoslaw <rad.piwowarczyk@gmail.com>
2021-02-01 20:46:15 +0100

    added SpecialitySDJpaService

---
+++ pet-clinic-data/src/main/java/spring/project/com/service/springDataJpa/SpecialitySDJpaService.java
@@ -0,0 +1,48 @@
+package spring.project.com.service.springDataJpa;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import spring.project.com.model.Speciality;
+import spring.project.com.repository.SpecialityRepository;
+import spring.project.com.service.SpecialityService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springDataJpa")
+public class SpecialitySDJpaService implements SpecialityService {
+
+    private final SpecialityRepository specialityRepository;
+
+    public SpecialitySDJpaService(SpecialityRepository specialityRepository) {
+        this.specialityRepository = specialityRepository;
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        Set<Speciality> specialities = new HashSet<>();
+        specialityRepository.findAll().forEach(specialities::add);
+        return specialities;
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return specialityRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return specialityRepository.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        specialityRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        specialityRepository.deleteById(id);
+    }
+}
