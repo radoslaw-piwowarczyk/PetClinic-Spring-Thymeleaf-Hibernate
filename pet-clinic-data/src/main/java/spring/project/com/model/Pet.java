fdf2250acfc0f3c864783fad2e389d6d84185958
radoslaw <rad.piwowarczyk@gmail.com>
2021-02-01 08:22:47 +0100

    made Owner,Pet Entity and add relations

--- pet-clinic-data/src/main/java/spring/project/com/model/Pet.java
+++ pet-clinic-data/src/main/java/spring/project/com/model/Pet.java
@@ -1,13 +1,22 @@
 package spring.project.com.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet  extends BaseEntity{
 
     private String name;
-    private PetType petType;
-    private Owner owner;
     private LocalDate birthDate;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
+    private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private Owner owner;
 
     public String getName() {
         return name;
