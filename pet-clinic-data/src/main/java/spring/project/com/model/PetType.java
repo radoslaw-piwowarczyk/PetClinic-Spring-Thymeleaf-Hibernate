bbcf9b138863786c074df1bd17394e61802926a5
radoslaw <rad.piwowarczyk@gmail.com>
2021-02-01 08:24:09 +0100

    made PetType Entity and add relations

--- pet-clinic-data/src/main/java/spring/project/com/model/PetType.java
+++ pet-clinic-data/src/main/java/spring/project/com/model/PetType.java
@@ -1,5 +1,10 @@
 package spring.project.com.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity{
 
     private String name;
