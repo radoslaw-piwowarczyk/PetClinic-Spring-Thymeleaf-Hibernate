6d77338eb23e0af8e3af743ba153cc03fdb318a2
radoslaw <rad.piwowarczyk@gmail.com>
2021-02-01 21:03:19 +0100

    created VisitServiceMap

---
+++ pet-clinic-data/src/main/java/spring/project/com/service/VisitService.java
@@ -0,0 +1,6 @@
+package spring.project.com.service;
+
+import spring.project.com.model.Visit;
+
+public interface VisitService extends CrudService<Visit,Long>{
+}
---
+++ pet-clinic-data/src/main/java/spring/project/com/service/map/VisitServiceMap.java
@@ -0,0 +1,44 @@
+package spring.project.com.service.map;
+
+import org.springframework.stereotype.Service;
+import spring.project.com.model.Visit;
+import spring.project.com.service.VisitService;
+
+import java.util.Set;
+
+@Service
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+
+        if (visit.getPet() == null
+                || visit.getPet().getId() == null
+                || visit.getPet().getOwner() == null
+                || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid visit");
+        }
+        return super.save(visit);
+    }
+}
