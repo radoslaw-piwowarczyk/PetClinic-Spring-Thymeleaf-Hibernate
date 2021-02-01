c95d5a8b25f0e0dc07139d44ed39a9b4fd5359d4
radoslaw <rad.piwowarczyk@gmail.com>
2021-02-01 19:46:59 +0100

    Made Entity of Visit and Pet and added relations beetwen them

--- pet-clinic-data/src/main/java/spring/project/com/model/Pet.java
+++ pet-clinic-data/src/main/java/spring/project/com/model/Pet.java
@@ -2,6 +2,8 @@ package spring.project.com.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -17,6 +19,9 @@ public class Pet  extends BaseEntity{
     @ManyToOne
     @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -48,5 +53,13 @@ public class Pet  extends BaseEntity{
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
--- pet-clinic-data/src/main/java/spring/project/com/model/Visit.java
+++ pet-clinic-data/src/main/java/spring/project/com/model/Visit.java
@@ -1,11 +1,17 @@
 package spring.project.com.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
-public class Visit {
+@Entity
+@Table(name = "visits")
+public class Visit extends BaseEntity{
 
     private LocalDate date;
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
